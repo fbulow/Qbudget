2d661b9c2c8f0f94a85c147c4d4c7d1ce29ea6c9
Fredrik Bülow <fredrik.bulow@gmail.com>
2017-12-09 17:52:47 +0100

    Refactored.

--- notes_test.py
+++ notes_test.py
@@ -2,16 +2,16 @@ import unittest
 
 Tc = unittest.TestCase
 
-def categoryKey(name):
+def _categoryKey(name):
     return name[3:]
 
 def getCategory(fcn):
-    key = categoryKey(fcn.__name__)
+    key = _categoryKey(fcn.__name__)
     def ret(self, year, month, person):
         return self.get((key, year, month, person),[])
     return ret
 def setCategory(fcn):
-    key = categoryKey(fcn.__name__)
+    key = _categoryKey(fcn.__name__)
     def ret(self, year, month, person, ammount, comment):
         self[(key, year, month, person)] = self.get(key, []) + [tuple([ammount, comment])]
     return ret
@@ -22,11 +22,13 @@ class Notes(dict):
     def addIncome(self, year, month, person, ammount, comment):pass
     @getCategory
     def getIncome(self, year, month, person):pass
+    
     @setCategory
     def addMutualExpense(self, year, month, person, ammount, comment):pass
     @getCategory
     def getMutualExpense(self, year, month, person):pass
 
+    
 class NoMixing(Tc):
     def test_logged(self):
         sut = Notes()
