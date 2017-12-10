3d758cc5299f82a652cd6d1a9e3bb770a831454e
Fredrik Bülow <fredrik.bulow@gmail.com>
2017-12-10 16:21:48 +0100

    Test TestTransaction/test_multipleItems.

--- notes_test.py
+++ notes_test.py
@@ -6,6 +6,37 @@ from notes import *
 def Month(*arg):
     return tuple(arg)
         
+class TestTransaction(Tc):
+    def test_multipleItems(self):
+        time = Month(2012,5)
+        
+        sut=Notes()
+        sut.addIncome(
+            time = time,
+            person = "Fredrik",
+            ammount = 6000.00,
+            comment = "CSNA")
+        
+        sut.addIncome(
+            time = time,
+            person = "Fredrik",
+            ammount = 3000.00,
+            comment = "CSNB")
+        
+        sut.addIncome(
+            time = time,
+            person = "Fredrik",
+            ammount = 3000.00,
+            comment = "CSNC")
+        
+        ret = sut.getIncome(
+            time = time,
+            person = "Fredrik")
+        
+        self.assertEqual(ret,
+                         [(6000.00, "CSNA"),
+                          (3000.00, "CSNB"),
+                          (3000.00, "CSNC")])
 
 class NoMixing(Tc):
     def test_logged(self):
